54bf4ec42e43b9366eaf2d148ce198565f7b5077
Ishindoutai <jseacello@gmail.com>
2025-04-09 00:11:49 -0300

    Salva progresso

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,10 +2,11 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFunctions } from 'firebase/functions';
 
+//https://z-users-manager-default-rtdb.firebaseio.com
 const firebaseConfig = {
   apiKey: "AIzaSyDfFtKwPNYnu2yTISr77l2zfScOrNpRmrU",
   authDomain: "z-users-manager.firebaseapp.com",
-  databaseURL: "https://z-users-manager-default-rtdb.firebaseio.com",
+  databaseURL: "https://127.0.0.1:5001",
   projectId: "z-users-manager",
   storageBucket: "z-users-manager.firebasestorage.app",
   messagingSenderId: "520825228121",
